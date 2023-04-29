c9c37d8e15cfb125f4deb251f21a1e3bb8bb7388
Jose <josemanuel21302@gmail.com>
2023-04-28 18:53:27 -0700

    finished routing

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,12 +1,15 @@
 
 
-export default function Packages() {
-
+export default function Packages({ packages }) {
+    const displayPackages = packages.map(eachPackageElement => <li>{eachPackageElement}</li>)
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
+                        <ul>
+                            {displayPackages}
+                        </ul>
                 </div>
                 
             </div>
